614035f0e28581e7c843715741a3e9dae584376c
Colono <av.colono@gmail.com>
2012-01-26 17:30:17 -0200

    build tools osx fix

--- tools/run.sh
+++ tools/run.sh
@@ -3,5 +3,5 @@
 echo Building SDL demo files
 rm cache.sdl.txt 2> /dev/null
 rm StringCache.txt 2> /dev/null
-./sexporter -i seeddemos.xml -p sdl -v
-cp seeddemos.h ../common/sdl/assets.h
+./sexporter -i assets.xml -p iph -v
+cp assets.h ../assets.h

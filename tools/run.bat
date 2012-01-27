1eefd6a94200ffaeec590fe0c33bca91981f3a53
fungos <fungos@gmail.com>
2012-01-27 01:26:28 -0200

    Generic makefile with OS detection

--- tools/run.bat
+++ tools/run.bat
@@ -2,7 +2,7 @@
 
 del cache.sdl.txt
 del StringCache.txt
-sexport -i assets.xml -p sdl
+sexporter.exe -i assets.xml -p sdl
 copy assets.h ..\assets.h"
 @echo Done!
 pause
--- tools/run.sh
+++ tools/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo Building SDL demo files
+PLAT=$1
+
+if [ "x$1" = "x" ]
+then
+	PLAT=sdl
+fi
+
+echo $PLAT
+
+echo Building assets
 rm cache.sdl.txt 2> /dev/null
 rm StringCache.txt 2> /dev/null
-./sexporter -i assets.xml -p iph -v
+./sexporter -i assets.xml -p $PLAT -v
 cp assets.h ../assets.h
